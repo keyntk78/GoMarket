fb6d7aa5f522031bc4a2d8a1fc6dc6f242b36a76
Nguyễn Tuấn Kiệt <101924064+keyntk78@users.noreply.github.com>
2023-02-03 07:58:30 +0700

    Init project

---
+++ GoMarket/Models/GoMarketDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GoMarket.Models
+{
+    public class GoMarketDbContext : IdentityDbContext<AppUser>
+    {
+        public GoMarketDbContext(DbContextOptions<GoMarketDbContext> options) : base(options) 
+        { 
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                var tableName = entityType.GetTableName();
+                if (tableName.StartsWith("AspNet"))
+                {
+                    entityType.SetTableName(tableName.Substring(6));
+                }
+            }
+        }
+    }
+}
